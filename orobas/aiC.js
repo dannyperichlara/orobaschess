daf88a15495a4287155a1378bef2ef9e282667f1
Danny Perich L <danny@Danny-2.local>
2021-08-25 04:40:43 -0300

    Fixes bug in Bad Moves Reductions

--- orobas/aiC.js
+++ orobas/aiC.js
@@ -1130,11 +1130,11 @@ AI.PVS = function (board, alpha, beta, depth, ply) {
             // Bad moves reductions
             if (!move.capture && AI.phase <= EARLY_ENDGAME) {
                 // console.log('no')
-                if (board.turn === WHITE && (board.board[move.to-17] === p || board.board[move.to-15] === p)) {
+                if (board.turn === WHITE && piece != P && (board.board[move.to-17] === p || board.board[move.to-15] === p)) {
                     R+=4
                 }
                 
-                if (board.turn === BLACK && (board.board[move.to+17] === P || board.board[move.to+15] === P)) {
+                if (board.turn === BLACK && piece != p && (board.board[move.to+17] === P || board.board[move.to+15] === P)) {
                     R+=4
                 }
             }
