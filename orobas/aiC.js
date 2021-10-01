f2bf9f1b87cf60f5e26935431db281da55c77c35
Danny Perich L <danny@Danny-2.local>
2021-10-01 10:57:58 -0300

    Version 2.1.3

--- orobas/aiC.js
+++ orobas/aiC.js
@@ -9,7 +9,7 @@ let rnd = new seedrandom('orobas1234', {global: true})
 console.log(Math.random())
 
 let AI = {
-    version: "2.1.2",
+    version: "2.1.3",
     totaldepth: 48,
     ttNodes: 0,
     iteration: 0,
